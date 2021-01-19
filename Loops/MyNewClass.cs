3a54d66c09e2a1654294eb67c1ae4ee06bd7fa26
Mehmethan <Mehmethan@DESKTOP-ELQMTII>
2021-01-19 03:12:00 +0300

    yorumlar silindi.

--- Loops/MyNewClass.cs
+++ Loops/MyNewClass.cs
@@ -7,12 +7,5 @@ namespace Loops
     class MyNewClass
     {
         public int test { get; set; }
-        // her zamman yeni branci masterdan oluştur. değişikliikleri yapıp commıt an dpush de
-        // öncekinde sanırım eski bir brancten yeni brach oluşmus ondan sapıtmış. normalde boyle olmuyor
-        //Anladım Teşekkür ederim ben branch olusturmak gerektıgını bılmıyordum
-        //normalde direk masterın uzerınde değişiklik yapıta gonderılıme durumuda var ama bu branch olayı uzun vadede daha sağlıklı
-        //gerı donme acısındanmı projeye
-        //aynen hemde her değişiklik kendi brancinde olunca masterda bulunan ana kod bozulmuyor taaki birleştirilene kadar
-        //Anladım benım en sonunda bırlestırmem gerekecekmı pekı evet onu nasıl yapacagım
     }
 }
