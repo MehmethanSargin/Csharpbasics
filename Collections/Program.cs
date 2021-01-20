a188197dec787431c1904530a8437f40f7bd9bfb
Mehmethan <Mehmethan@DESKTOP-ELQMTII>
2021-01-20 23:54:25 +0300

    oop

---
+++ Collections/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> isimler = new List<string> { "Mehmethan", "Kaan", "Emre", "Yunus emre" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[3]);
+            isimler.Add("Kader");
+            Console.WriteLine(isimler[4]);
+
+        }
+    }
+}
